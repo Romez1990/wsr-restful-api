89677e089d62ec2ebc3de80df6be46ec8cb22db6
Romez1990 <Romez1990@yandex.ru>
2018-10-22 08:29:18 +0300

    Finish creation product with upload image on server directory

--- API/index.php
+++ API/index.php
@@ -1,16 +1,11 @@
 <div id="text">
 <?php
-
 $url = explode('/', key($_GET));
-
 if (empty($url[0])) return;
 
 $class_name = $url[0];
 require($class_name . '.php');
 $class = new $class_name($url, $_GET, $_POST);
-echo $class->response;
-//*/
-
 ?>
 </div>
 
--- API/product.php
+++ API/product.php
@@ -2,53 +2,61 @@
 require_once 'database.php';
 
 class product extends database {
-	public function __construct($url, $get, $post) {
+	public function __construct($url) {
 		parent::__construct();
 		
-		if (count($post) > 0) {
+		if (count($_POST) > 0) {
 			// POST
-			$title =          $post['title'];
-			$manufacturer =   $post['manufacturer'];
-			$text =           $post['text'];
-			$tags =           $post['tags'];
-			$image =          $post['image'];
+			$title = $_POST['title'];
+			$manufacturer = $_POST['manufacturer'];
+			$text = $_POST['text'];
+			$tags = $_POST['tags'];
+			$image = $_FILES['image'];
 			
-			$title = 'Crap10';
-			$manufacturer = 'Me';
-			$text = 'its a good product';
-			$tags = 'good';
-			$image = 'API/product_images';
-			
-			$res = $this->db->query("SELECT * FROM `product` WHERE `title` = '$title'");
-			
-			if ($res->num_rows > 0) {
-				$result['status code'] = 400;
-				$result['status text'] = 'Creating error';
-				$result['body'] = array(
-					('status') => false,
-					('message') => array(
-						('title') => 'already exists'
-					)
-				);
-			} else {
-				$date = date('H:i d.m.Y');
-				$res = $this->db->query("INSERT INTO `product` (`title`, `manufacturer`, `text`, `tags`, `image`, `date_create`) VALUES ('$title', '$manufacturer', '$text', '$tags', '$image', '$date')");
-				
-				$result['status code'] = 201;
-				$result['status text'] = 'Successful creation';
-				$result['body'] = array(
-					('status') => true,
-					('post_id') => 0
-				);
+			// Already exists check
+			$res = $this->db->query("SELECT COUNT(`title`) AS `count` FROM `product` WHERE `title` = '$title'");
+			if ($res->fetch_assoc()['count'] > 0) {
+				// Already exists
+				$response['status code'] = 400;
+				$response['status text'] = 'Creating error';
+				$response['body']['status'] = false;
+				$response['body']['message']['title'] = 'already exists';
 			}
 			
-		} elseif (count($get) > count($url)) {
+			// Invalid file format check
+			if ($image['size'] > pow(2, 20) || $image['type'] !== 'image/png' && $image['type'] !== 'image/jpeg') {
+				if ($response['status code'] !== 400) {
+					$response['status code'] = 400;
+					$response['status text'] = 'Creating error';
+					$response['body']['status'] = false;
+				}
+				$response['body']['message']['image'] = 'invalid file format';
+			}
+			
+			// If there is no errors
+			if (!$response['status code']) {
+				// Upload image
+				$upload_dir = 'W:\domains\1_Module_RESTful_API\API\product_images';
+				$upload_path = $upload_dir . '\\' . $image['name'];
+				$result_file = move_uploaded_file($image['tmp_name'], $upload_path);
+				
+				$now = date('H:i d.m.Y');
+				$result_db = $this->db->query("INSERT INTO `product` (`title`, `manufacturer`, `text`, `tags`, `image`, `date_of_creation`) VALUES ('$title', '$manufacturer', '$text', '$tags', '$upload_path', '$now')");
+				
+				if ($result_file && $result_db) {
+					// Successful
+					$response['status code'] = 201;
+					$response['status text'] = 'Successful creation';
+					$response['body']['status'] = true;
+					$response['body']['post_id'] = $this->db->query("SELECT `id` FROM `product` WHERE `title` = '$title'")->fetch_assoc()['id'];
+				}
+			}
+			
+		} elseif (count($_GET) > count($url)) {
 			// GET
 			
 		}
 		
-		$this->response = json_encode($result);
+		echo json_encode($response);
 	}
-	
-	public $response;
 }
