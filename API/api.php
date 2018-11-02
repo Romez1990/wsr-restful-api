8bf2f2be712318fb4eb79fc67f4621952a606e1e
Romez1990 <Romez1990@yandex.ru>
2018-11-02 22:06:43 +0300

    Refactor a few moments

--- API/api.php
+++ API/api.php
@@ -1,16 +1,8 @@
-<title>Response</title>
-<div id="response">
-	<?php
-	$url = explode('/', key($_GET));
-	if (empty($url[0])) return;
-	
-	$class_name = $url[0];
-	require $class_name . '.php';
-	$class = new $class_name($url);
-	$class->db->close();
-	?>
-</div>
+<?php
+$url = explode('/', key($_GET));
+if (empty($url[0])) return;
 
-<script>
-	console.log(JSON.parse(document.getElementById('response').innerText));
-</script>
+$class_name = $url[0];
+require $class_name . '.php';
+$class = new $class_name($url);
+$class->db->close();
--- API/api2.php
+++
@@ -1,8 +0,0 @@
-<?php
-$url = explode('/', key($_GET));
-if (empty($url[0])) return;
-
-$class_name = $url[0];
-require $class_name . '.php';
-$class = new $class_name($url);
-$class->db->close();
