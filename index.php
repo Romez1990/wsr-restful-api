e1ae1d26340264eda8ae1e6af679809897959d01
Romez <Romez1990@yandex.ru>
2018-11-08 16:01:29 +0300

    Some change

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 <head>
 	<title>API</title>
 	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta name="viewport"
+	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<style>
 		a {
@@ -22,12 +23,9 @@
 <body>
 
 <?php
-
-$files = array_diff(scandir('html'), array('.', '..', 'css'));
-
+$files = array_diff(scandir('html'), array('.', '..', 'style.css'));
 foreach ($files as $file)
 	echo "<a href=\"html/$file\">$file</a>";
-
 ?>
 
 </body>
