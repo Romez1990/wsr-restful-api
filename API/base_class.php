fbebf0957b1c845185d6a36305a6d31f9826aa5f
Romez <Romez1990@yandex.ru>
2018-11-08 16:40:26 +0300

    Start

--- API/base_class.php
+++ API/base_class.php
@@ -1,20 +1,42 @@
 <?php
 
 class base_class {
+	protected $db;
+	protected $headers;
+	public $method;
+	
 	public function __construct() {
-		$this->db = new mysqli('127.0.0.1', 'root', '', '1_module_restful_api');
-		
-		// Check connection
-		if ($this->db->connect_error) {
-			die("Connection failed: " . $this->db->connect_error);
-		}
-		
+		$this->db = new mysqli('127.0.0.1', 'root', '', 'module1');
 		$this->headers = getallheaders();
 		$this->method = $_SERVER['REQUEST_METHOD'];
 	}
 	
-	public $db;
-	public $headers;
-	public $method;
+	protected $response;
+	
+	protected function set_response($status_code, $status_text, $status, $body) {
+		$this->response['status code'] = $status_code;
+		$this->response['status text'] = $status_text;
+		if ($body !== null) {
+			$this->response['body'] = $body;
+		}
+		if ($status !== null) {
+			$this->response['body']['status'] = $status;
+		}
+	}
+	
+	protected function print_response() {
+		echo json_encode($this->response, JSON_UNESCAPED_UNICODE);
+		$this->db->close();
+	}
+	
+	protected function check_authorized() {
+		$token = $this->headers['Authorization'];
+		if ($this->db->query("SELECT COUNT(*) AS `count` FROM `session` WHERE `token` = '$token'")->fetch_assoc()['count'] === '0') {
+			$this->set_response(401, 'Unauthorized', null, array('message' => 'Unauthorized'));
+			return false;
+		} else {
+			return true;
+		}
+	}
 	
 }
---
+++ API/index.php
@@ -0,0 +1,23 @@
+<?php
+
+$url = explode('/', key($_GET));
+
+if (!file_exists($url[0] . '.php')) return;
+require_once $url[0] . '.php';
+$class = new $url[0]();
+
+switch ($url[0]) {
+	case 'auth':
+		if ($class->method === 'POST' && count($url) === 1) {
+			$class->auth();
+		}
+		break;
+
+	case 'product':
+		if ($class->method === 'POST' && count($url) === 1) {
+			$class->creation();
+		} elseif ($class->method === 'POST' && count($url) === 2 && is_numeric($url[1])) {
+			$class->editing($url[1]);
+		}
+		break;
+}
